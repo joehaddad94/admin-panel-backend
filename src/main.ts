6c0c61d372a20c58147e6df20f281c50927edb80
Joe Haddad <joe@sefactory.io>
2024-05-28 13:08:32 +0300

    Editing vercel.json

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,18 @@ import { AppModule } from './app.module';
 import { initOpenApi } from '@core/config/documentation/swagger';
 
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule, {
-      cors: {
-        origin: '*',
-      },
-    });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+  });
 
-    const port = process.env.SERVER_PORT || 3000;
+  const port = process.env.SERVER_PORT || 3000;
 
-    console.log(
-      `Server running on port ${port} || env: ${process.env.NODE_ENV}`,
-    );
+  console.log(`Server running on port ${port} || env: ${process.env.NODE_ENV}`);
 
-    initOpenApi(app);
+  initOpenApi(app);
 
-    await app.listen(port);
-  } catch (error) {
-    return error;
-  }
+  await app.listen(port);
 }
 bootstrap();
