49e33a82c93d592ed4aa0732b8a478a10c364108
Joe Haddad <joe@sefactory.io>
2024-05-30 19:54:26 +0300

    Editing report mediator

--- src/domain/reports/report.mediator.ts
+++ src/domain/reports/report.mediator.ts
@@ -10,6 +10,7 @@ import { Application } from '../../core/data/database/entities/application.entit
 import { GlobalEntities } from '../../core/data/types';
 import { catcher } from '../../core/helpers/operation';
 import { throwNotFound } from '../../core/settings/base/errors/errors';
+import { ApplicationUser } from 'src/core/data/database/relations/application-user.entity';
 
 @Injectable()
 export class ReportMediator {
@@ -127,39 +128,72 @@ export class ReportMediator {
     });
   };
 
-  informationReport = async (
-    filtersDto: FiltersDto,
-    page: number = 1,
-    pageSize: number = 100,
-  ) => {
+  // informationReport = async (
+  //   filtersDto: FiltersDto,
+  //   page: number = 1,
+  //   pageSize: number = 100,
+  // ) => {
+  //   return catcher(async () => {
+  //     const {
+  //       fromDate,
+  //       toDate,
+  //       page: dtoPage,
+  //       pageSize: dtoPageSize,
+  //     } = filtersDto;
+
+  //     const currentPage = dtoPage ?? page;
+  //     const currentPageSize = dtoPageSize ?? pageSize;
+
+  //     const options: GlobalEntities[] = ['applicationInfo', 'informationUser'];
+  //     const whereConditions: any = {};
+
+  //     if (fromDate && toDate) {
+  //       whereConditions.created_at = Between(fromDate, toDate);
+  //     } else if (fromDate) {
+  //       whereConditions.created_at = MoreThanOrEqual(fromDate);
+  //     } else if (toDate) {
+  //       whereConditions.created_at = LessThanOrEqual(toDate);
+  //     }
+
+  //     const [information, total] = await this.informationService.findAndCount(
+  //       whereConditions,
+  //       options,
+  //       undefined,
+  //       (currentPage - 1) * currentPageSize,
+  //       currentPageSize,
+  //     );
+
+  //     throwNotFound({
+  //       entity: 'informationReport',
+  //       errorCheck: !information,
+  //     });
+
+  //     return {
+  //       information,
+  //       total,
+  //       page: currentPage,
+  //       pageSize: currentPageSize,
+  //     };
+  //   });
+  // };
+
+  informationReport = async (filtersDto: FiltersDto) => {
     return catcher(async () => {
-      const {
-        fromDate,
-        toDate,
-        page: dtoPage,
-        pageSize: dtoPageSize,
-      } = filtersDto;
+      const { fromDate, toDate } = filtersDto;
 
-      const currentPage = dtoPage ?? page;
-      const currentPageSize = dtoPageSize ?? pageSize;
-
-      const options: GlobalEntities[] = ['applicationInfo', 'informationUser'];
-      const whereConditions: any = {};
+      const infoOptions: GlobalEntities[] = ['informationUser'];
+      const infoWhereConditions: any = {};
 
       if (fromDate && toDate) {
-        whereConditions.created_at = Between(fromDate, toDate);
+        infoWhereConditions.created_at = Between(fromDate, toDate);
       } else if (fromDate) {
-        whereConditions.created_at = MoreThanOrEqual(fromDate);
+        infoWhereConditions.created_at = MoreThanOrEqual(fromDate);
       } else if (toDate) {
-        whereConditions.created_at = LessThanOrEqual(toDate);
+        infoWhereConditions.created_at = LessThanOrEqual(toDate);
       }
 
-      const [information, total] = await this.informationService.findAndCount(
-        whereConditions,
-        options,
-        undefined,
-        (currentPage - 1) * currentPageSize,
-        currentPageSize,
+      const information = await this.informationService.findMany(
+        infoWhereConditions,
       );
 
       throwNotFound({
@@ -167,12 +201,139 @@ export class ReportMediator {
         errorCheck: !information,
       });
 
-      return {
-        information,
-        total,
-        page: currentPage,
-        pageSize: currentPageSize,
-      };
+      const applicationOptions: GlobalEntities[] = [
+        'applicationUser',
+        'applicationProgram',
+      ];
+
+      const applications = await this.applicationService.findMany(
+        {},
+        applicationOptions,
+      );
+
+      throwNotFound({
+        entity: 'informationReport',
+        errorCheck: !applications,
+      });
+
+      const applicationMap = new Map(applications.map((app) => [app.id, app]));
+      console.log(
+        '🚀 ~ ReportMediator ~ returncatcher ~ applicationMap:',
+        applicationMap,
+      );
+
+      // const combinedData = information.flatMap((info) => {
+      //   if (info.applicationInfo && info.applicationInfo.length > 0) {
+      //     return info.applicationInfo.map((appInfo) => {
+      //       const appId = typeof appInfo === 'number' ? appInfo : appInfo.id;
+      //       const application = applicationMap.get(appId);
+      //       const applicationUser = application?.applicationUser?.[0]?.user;
+
+      //       return {
+      //         sef_id: applicationUser?.sef_id || info.sef_id,
+      //         username: applicationUser?.username || '',
+      //         email: applicationUser?.email || info.email,
+      //         first_name: info.first_name,
+      //         middle_name: info.middle_name,
+      //         last_name: info.last_name,
+      //         mother_maiden_first: info.mother_maiden_first,
+      //         mother_maiden_last: info.mother_maiden_last,
+      //         gender: info.gender,
+      //         dob: info.dob,
+      //         mobile: info.mobile,
+      //         country_origin: info.country_origin,
+      //         country_residence: info.country_residence,
+      //         residency_status: info.residency_status,
+      //         district: info.district,
+      //         governate: info.governate,
+      //         marital_status: info.marital_status,
+      //         type_of_disability: info.type_of_disability,
+      //         disability: info.disability,
+      //         employment_situation: info.employment_situation,
+      //         which_social: info.which_social,
+      //         terms_conditions: info.terms_conditions,
+      //         degree_type: info.degree_type,
+      //         status: info.status,
+      //         institution: info.institution,
+      //         field_of_study: info.field_of_study,
+      //         major_title: info.major_title,
+      //         createdAt: info.created_at,
+      //         program_id:
+      //           application?.applicationProgram?.[0]?.program.id || '',
+      //         program_name:
+      //           application?.applicationProgram?.[0]?.program.program_name ||
+      //           '',
+      //         abbreviation:
+      //           application?.applicationProgram?.[0]?.program.abbreviation ||
+      //           '',
+      //         passed_screening: application?.passed_screening || '',
+      //         passed_screening_date: application?.passed_screening_date || '',
+      //         passed_exam: application?.passed_exam || '',
+      //         passed_exam_date: application?.passed_exam_date || '',
+      //         passed_interview_date: application?.passed_interview_date || '',
+      //         passed_interview: application?.passed_interview || '',
+      //         enrolled: application?.enrolled || '',
+      //         remarks: application?.remarks || '',
+      //         extras: application?.extras || '',
+      //       };
+      //     });
+      //   } else {
+      //     return [
+      //       {
+      //         sef_id: info.sef_id,
+      //         username: '',
+      //         email: info.email,
+      //         first_name: info.first_name,
+      //         middle_name: info.middle_name,
+      //         last_name: info.last_name,
+      //         mother_maiden_first: info.mother_maiden_first,
+      //         mother_maiden_last: info.mother_maiden_last,
+      //         gender: info.gender,
+      //         dob: info.dob,
+      //         mobile: info.mobile,
+      //         country_origin: info.country_origin,
+      //         country_residence: info.country_residence,
+      //         residency_status: info.residency_status,
+      //         district: info.district,
+      //         governate: info.governate,
+      //         marital_status: info.marital_status,
+      //         type_of_disability: info.type_of_disability,
+      //         disability: info.disability,
+      //         employment_situation: info.employment_situation,
+      //         which_social: info.which_social,
+      //         terms_conditions: info.terms_conditions,
+      //         degree_type: info.degree_type,
+      //         status: info.status,
+      //         institution: info.institution,
+      //         field_of_study: info.field_of_study,
+      //         major_title: info.major_title,
+      //         createdAt: info.created_at,
+      //         program_id: '',
+      //         program_name: '',
+      //         abbreviation: '',
+      //         passed_screening: '',
+      //         passed_screening_date: '',
+      //         passed_exam: '',
+      //         passed_exam_date: '',
+      //         passed_interview_date: '',
+      //         passed_interview: '',
+      //         enrolled: '',
+      //         remarks: '',
+      //         extras: '',
+      //       },
+      //     ];
+      //   }
+      // });
+
+      // combinedData.sort((a, b) => a.sef_id.localeCompare(b.sef_id));
+
+      // return {
+      //   combinedData,
+      // };
+
+      const applicationMapObject = Object.fromEntries(applicationMap);
+
+      return { information, applicationMap: applicationMapObject };
     });
   };
 
