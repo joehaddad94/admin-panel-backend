f93d2c2496a13a96b78fd10368612aebdf5471a2
Joe Haddad <joehaddad94@gmail.com>
2024-09-16 18:20:00 +0300

    Edit <> Adjusted validateThresholds function

--- src/core/helpers/validateThresholds.ts
+++ src/core/helpers/validateThresholds.ts
@@ -7,26 +7,37 @@ export function validateThresholdEntity(threshold: Threshold): void {
 
   if (
     threshold.exam_passing_grade === null ||
-    threshold.exam_passing_grade === undefined
+    threshold.exam_passing_grade === undefined ||
+    threshold.exam_passing_grade === 0
   ) {
     invalidProperties.push('Exam Passing Grade');
   }
   if (
     threshold.primary_passing_grade === null ||
-    threshold.primary_passing_grade === undefined
+    threshold.primary_passing_grade === undefined ||
+    threshold.primary_passing_grade === 0
   ) {
     invalidProperties.push('Primary Passing Grade');
   }
   if (
     threshold.secondary_passing_grade === null ||
-    threshold.secondary_passing_grade === undefined
+    threshold.secondary_passing_grade === undefined ||
+    threshold.secondary_passing_grade === 0
   ) {
     invalidProperties.push('Secondary Passing Grade');
   }
-  if (threshold.weight_tech === null || threshold.weight_tech === undefined) {
+  if (
+    threshold.weight_tech === null ||
+    threshold.weight_tech === undefined ||
+    threshold.weight_tech === 0
+  ) {
     invalidProperties.push('Weight Tech');
   }
-  if (threshold.weight_soft === null || threshold.weight_soft === undefined) {
+  if (
+    threshold.weight_soft === null ||
+    threshold.weight_soft === undefined ||
+    threshold.weight_soft === 0
+  ) {
     invalidProperties.push('Weight Soft');
   }
 
