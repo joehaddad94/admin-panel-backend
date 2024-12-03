4c5a7c146498be140aeb6e44f93a5feceb96466d
Joe Haddad <joehaddad94@gmail.com>
2024-12-03 14:06:19 +0200

    Edit <> Edited sending emails through threads not one by one

--- src/core/helpers/sendMail.ts
+++ src/core/helpers/sendMail.ts
@@ -3,6 +3,40 @@ import { Logger } from '@nestjs/common';
 
 const logger = new Logger('EmailHelper');
 
+// export const sendBulkEmails = async (
+//   mailerService: MailerService,
+//   emails: { email: string; name: string }[],
+//   template: string,
+//   subject: string,
+//   templateVariables?: Record<string, any>,
+// ) => {
+//   const emailPromises = emails.map(async ({ email, name }) => {
+//     // const link = `${process.env.VERIFY_CLIENT_URL}?key=${resetToken}&email=${email}`;
+
+//     try {
+//       const result = await mailerService.sendMail({
+//         from: '"SE Factory" <noreply@example.com>',
+//         to: email,
+//         subject,
+//         template,
+//         context: {
+//           name,
+//           //   link,
+//           ...templateVariables,
+//         },
+//       });
+
+//       logger.log(`Email sent to ${email}: ${result.messageId}`);
+//       return { email, result };
+//     } catch (error) {
+//       logger.error(`Failed to send email to ${email}`, error.stack);
+//       return { email, error: error.message };
+//     }
+//   });
+
+//   return Promise.all(emailPromises);
+// };
+
 export const sendBulkEmails = async (
   mailerService: MailerService,
   emails: { email: string; name: string }[],
@@ -10,18 +44,38 @@ export const sendBulkEmails = async (
   subject: string,
   templateVariables?: Record<string, any>,
 ) => {
-  const emailPromises = emails.map(async ({ email, name }) => {
-    // const link = `${process.env.VERIFY_CLIENT_URL}?key=${resetToken}&email=${email}`;
+  if (emails.length > 1) {
+    try {
+      const result = await mailerService.sendMail({
+        from: '"SE Factory" <noreply@example.com>',
+        to: 'selection@sefactory.io',
+        cc: 'charbeld@sefactory.io',
+        bcc: emails.map(({ email }) => email),
+        subject,
+        template,
+        context: {
+          ...templateVariables,
+        },
+      });
 
+      logger.log(`Bulk email sent with BCC to ${emails.length} recipients.`);
+      return emails.map(({ email }) => ({ email, result }));
+    } catch (error) {
+      logger.error('Failed to send bulk email with BCC', error.stack);
+      return emails.map(({ email }) => ({ email, error: error.message }));
+    }
+  }
+
+  const emailPromises = emails.map(async ({ email, name }) => {
     try {
       const result = await mailerService.sendMail({
         from: '"SE Factory" <noreply@example.com>',
         to: email,
+        cc: 'charbeld@sefactory.io',
         subject,
         template,
         context: {
           name,
-          //   link,
           ...templateVariables,
         },
       });
